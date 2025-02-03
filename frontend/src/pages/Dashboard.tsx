4532db1d48441437a8b33d4eca35eaa917bfa67c
sahils38 <sahilsaras38@gmail.com>
2025-02-03 19:51:20 +0530

    integrating the backend with frontend

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -96,7 +96,7 @@ const Dashboard = () => {
         platform,
       };
 
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://socialscan-production.up.railway.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
